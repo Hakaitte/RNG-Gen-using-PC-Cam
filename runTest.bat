0b87b847de706d273336adaa5ed8f0afca5a4c34
Th3J0jC0p <jakub.ozmina@student.put.poznan.pl>
2025-05-13 01:01:51 +0200

    add tests

---
+++ runTest.bat
@@ -0,0 +1,5 @@
+@echo off
+C:\Users\0ls0\AppData\Local\Programs\Python\Python313\python.exe d:\dev\newDev\RNG-Gen-using-PC-Cam\rnggen.py
+cd ./NIST-Statistical-Test-Suite/sts
+start assess 1000000
+cd ../../

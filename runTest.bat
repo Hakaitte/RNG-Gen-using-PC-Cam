4a64a59bf4f3738540593b5776f107bf414296bb
Th3J0jC0p <jakub.ozmina@student.put.poznan.pl>
2025-05-13 01:14:36 +0200

    changed python paths

--- runTest.bat
+++ runTest.bat
@@ -1,5 +1,13 @@
 @echo off
-C:\Users\0ls0\AppData\Local\Programs\Python\Python313\python.exe d:\dev\newDev\RNG-Gen-using-PC-Cam\rnggen.py
+
+for /f "usebackq tokens=*" %%i in (".env") do (
+    for /f "tokens=1,2 delims==" %%a in ("%%i") do (
+        if "%%a"=="pythonDir" set pythonDir=%%b
+        if "%%a"=="projectDir" set projectDir=%%b
+    )
+)
+
+%pythonDir% %projectDir%
 cd ./NIST-Statistical-Test-Suite/sts
 start assess 1000000
 cd ../../
